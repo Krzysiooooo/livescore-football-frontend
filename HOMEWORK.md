67628d5dc478e06a2b6fe7b420b0b6e077a1f73e
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-03-19 20:02:55 +0100

    Pagination WIP

---
+++ HOMEWORK.md
@@ -0,0 +1,4 @@
+Napisać rozwiązanie ktore w LeaguesPage wyrenderuje 5x Pagination item
+
+PODPOWIEDZ
+[1,2,3,4,5].map((page)=>{})
--- src/components/Pages/LeaguesPage.js
+++ src/components/Pages/LeaguesPage.js
@@ -1,31 +1,49 @@
 import React from 'react'
 import MockApi from "../../services/MockApi";
 import BriefCard from "../BriefCard/BriefCard";
-import {Col, Row} from "react-bootstrap";
+import {Col, Row, Pagination} from "react-bootstrap";
 
 class LeaguesPage extends React.Component {
+    items = [];
 
     constructor() {
         super();
         this.state = {
-            leagues: []
+            leagues: [],
+            activePage: 1
         };
 
         MockApi.getLeagues().then((leagues) => {
             this.setState({leagues: leagues});
-        })
+        });
+        this.onPageChange = this.onPageChange.bind(this);
     }
+
+    onPageChange(event){
+        const page = parseInt(event.target.innerText);
+        this.setState({activePage:page});
+    }
+
 
     renderLeague(league) {
         return <Col xs={3} className= "mb-4" key={league.league_id} ><BriefCard key={league.league_id} data-league={league}></BriefCard></Col>
     }
 
-    render() {
+    render(){
         return (<div>
             <h2>Leagues</h2>
             <Row>
                 {this.state.leagues.map(this.renderLeague)}
             </Row>
+            <div>
+                <Pagination>
+                    <Pagination.Prev></Pagination.Prev>
+                    /* TODO */
+                        <Pagination.Item key={1} active={1 === this.state.activePage} onClick ={this.onPageChange}>{1}</Pagination.Item>
+                        <Pagination.Item key={2} active={2 === this.state.activePage} onClick ={this.onPageChange}>{2}</Pagination.Item>
+                    <Pagination.Next></Pagination.Next>
+                </Pagination>
+            </div>
         </div>)
     }
 }
