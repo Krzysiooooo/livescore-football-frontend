6de1817c5225ec2260f996ac5f0d99a47b45db57
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-03-19 19:16:25 +0100

    Brief cards on LeaguesPage

--- src/components/Pages/LeaguesPage.js
+++ src/components/Pages/LeaguesPage.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import MockApi from "../../services/MockApi";
+import BriefCard from "../BriefCard/BriefCard";
+import {Col, Row} from "react-bootstrap";
 
 class LeaguesPage extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             leagues: []
         };
 
         MockApi.getLeagues().then((leagues) => {
-            this.setState({leages: leagues});
+            this.setState({leagues: leagues});
         })
     }
 
-    renderLeague(){
-        // todo return JSX with league element
+    renderLeague(league) {
+        return <Col xs={3} className= "mb-4" key={league.league_id} ><BriefCard key={league.league_id} data-league={league}></BriefCard></Col>
     }
 
     render() {
         return (<div>
             <h2>Leagues</h2>
-            (this.state.leagues.map(renderLeague))
+            <Row>
+                {this.state.leagues.map(this.renderLeague)}
+            </Row>
         </div>)
     }
 }
