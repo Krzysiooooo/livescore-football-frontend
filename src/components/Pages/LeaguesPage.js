9b2c51cfcafddad19b9801be66b1e9d1b4b91650
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-03-26 18:19:49 +0100

    Pagination improvement

--- src/components/Pages/LeaguesPage.js
+++ src/components/Pages/LeaguesPage.js
@@ -19,17 +19,18 @@ class LeaguesPage extends React.Component {
         this.onPageChange = this.onPageChange.bind(this);
     }
 
-    onPageChange(event){
+    onPageChange(event) {
         const page = parseInt(event.target.innerText);
-        this.setState({activePage:page});
+        this.setState({activePage: page});
     }
 
 
     renderLeague(league) {
-        return <Col xs={3} className= "mb-4" key={league.league_id} ><BriefCard key={league.league_id} data-league={league}></BriefCard></Col>
+        return <Col xs={3} className="mb-4" key={league.league_id}><BriefCard key={league.league_id}
+                                                                              data-league={league}></BriefCard></Col>
     }
 
-    render(){
+    render() {
         return (<div>
             <h2>Leagues</h2>
             <Row>
@@ -38,9 +39,8 @@ class LeaguesPage extends React.Component {
             <div>
                 <Pagination>
                     <Pagination.Prev></Pagination.Prev>
-                    /* TODO */
-                        <Pagination.Item key={1} active={1 === this.state.activePage} onClick ={this.onPageChange}>{1}</Pagination.Item>
-                        <Pagination.Item key={2} active={2 === this.state.activePage} onClick ={this.onPageChange}>{2}</Pagination.Item>
+                    {[1, 2, 3, 4, 5].map(i => <Pagination.Item key={i} active={i === this.state.activePage}
+                                                               onClick={this.onPageChange}>{i}</Pagination.Item>)}
                     <Pagination.Next></Pagination.Next>
                 </Pagination>
             </div>
