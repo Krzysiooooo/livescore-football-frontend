f1f78ff767097ce661730e6371c58a8f12694ba1
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-05-01 11:11:55 +0200

    wip input group debuging

--- src/components/Pages/LeaguesPage.js
+++ src/components/Pages/LeaguesPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import BackendApi from "../../services/BackendApi";
 import BriefCard from "../BriefCard/BriefCard";
-import {Col, Row, Pagination} from "react-bootstrap";
+import {Col, Row, Pagination, Form, InputGroup} from "react-bootstrap";
 
 class LeaguesPage extends React.Component {
     items = [];
@@ -11,7 +11,8 @@ class LeaguesPage extends React.Component {
         this.state = {
             leagues: [],
             activePage: 1,
-            paginationItems: [1, 2, 3, 4, 5]
+            paginationItems: [1, 2, 3, 4, 5],
+            searchValue: ""
         };
 
         BackendApi.getLeagues().then((leagues) => {
@@ -30,25 +31,51 @@ class LeaguesPage extends React.Component {
     }
 
     renderLeague(league) {
-        return <Col xs={3} className="mb-4" key={league.league_id}><BriefCard key={league.league_id} data-league={league}></BriefCard></Col>
+        return <Col xs={3} className="mb-4" key={league.league_id}><BriefCard key={league.league_id}
+                                                                              data-league={league}></BriefCard></Col>
     }
 
     renderPaginationItem(i) {
         return <Pagination.Item key={i} active={i === this.state.activePage}
                                 onClick={this.onPageChange}>{i}</Pagination.Item>;
     }
+    search(){
+
+    }
 
     render() {
         return (<div>
-            <h2>Leagues</h2>
+            <h2 className="pb-4">Leagues</h2>
             <Row>
-                <Col className="d-flex justify-content-center mb-4"><Pagination>{this.state.paginationItems.map(this.renderPaginationItem)}</Pagination></Col>
+                <Col xs="12">
+                    <Form>
+                        <Form.Row>
+                            <Form.Group as={Col} md="4">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control
+                                    type="text"
+                                    name="search"
+                                    placeholder="Search league"
+                                    aria-describedby="inputGroupPrepend"
+                                    value={this.state.searchValue}
+                                    onChange={this.search}/>
+                            </Form.Group>
+                        </Form.Row>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                <Col
+                    className="d-flex justify-content-center mb-4"><Pagination>{this.state.paginationItems.map(this.renderPaginationItem)}</Pagination></Col>
             </Row>
             <Row>
                 {this.state.leagues.map(this.renderLeague)}
             </Row>
             <Row>
-                <Col className="d-flex justify-content-center mt-4"><Pagination>{this.state.paginationItems.map(this.renderPaginationItem)}</Pagination></Col>
+                <Col
+                    className="d-flex justify-content-center mt-4"><Pagination>{this.state.paginationItems.map(this.renderPaginationItem)}</Pagination></Col>
             </Row>
         </div>)
     }
