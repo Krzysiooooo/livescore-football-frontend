886c8d46956434a4efcb63c9911672f04f3cb3b4
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-08-30 11:33:49 +0200

    TeamPage responsive view fix

--- src/components/Pages/TeamPage.js
+++ src/components/Pages/TeamPage.js
@@ -3,6 +3,7 @@ import BackendApi from "../../services/BackendApi";
 import {Col} from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
+import './TeamPage.css';
 
 class TeamPage extends React.Component {
 
@@ -16,13 +17,13 @@ class TeamPage extends React.Component {
     }
 
     render() {
-        return <Row>
+        return <Row id="team-page">
             <Col>
                 <h1>{this.state.team.name}</h1>
                 <p>Since: {this.state.team.founded} <br/>{this.state.team.country}, {this.state.team.venue_city}</p>
             </Col>
             <Col>
-                <Image src={this.state.team.logo}></Image>
+                <Image src={this.state.team.logo} className="team-logo"></Image>
             </Col>
         </Row>
     }
