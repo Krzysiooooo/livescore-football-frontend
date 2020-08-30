e845f409c255d6a924af4e5a57beefc5519e98ea
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-08-30 11:43:44 +0200

    InlineFixture responsive view fix

--- src/components/InlineFixture/InlineFixture.js
+++ src/components/InlineFixture/InlineFixture.js
@@ -12,15 +12,15 @@ class InlineFixture extends React.Component{
    render() {
         return <div key={this.props.fixture.fixture_id} className="inline-fixture">
            <Row>
-               <Col>
+               <Col xs={4}>
                    <Image src={this.props.fixture.homeTeam.logo} className="float-left"></Image>
                    <h3>{this.props.fixture.homeTeam.team_name}</h3>
                </Col>
-               <Col className="text-center score">
+               <Col className="text-center score" xs={4}>
                    <h3>{this.props.fixture.goalsHomeTeam} : {this.props.fixture.goalsAwayTeam}</h3>
                    <span title={this.props.fixture.event_date}>{this.getHumanDate(this.props.fixture.event_date)}</span>
                </Col>
-               <Col className="text-right">
+               <Col className="text-right" xs={4}>
                    <Image src={this.props.fixture.awayTeam.logo} className="float-right"></Image>
                    <h3>{this.props.fixture.awayTeam.team_name}</h3>
                </Col>
