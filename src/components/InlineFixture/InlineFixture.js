7cbd2d4ff9111a71e2d0e5881fee2b10e2a22075
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2021-02-09 17:55:56 +0100

    Website logo + favicons + title

--- src/components/InlineFixture/InlineFixture.js
+++ src/components/InlineFixture/InlineFixture.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Col, Image, Row} from "react-bootstrap";
+import {Col, Image, Row, ProgressBar} from "react-bootstrap";
 import './InlineFixture.css'
 import moment from "moment";
 
@@ -16,7 +16,7 @@ class InlineFixture extends React.Component{
                    <Image src={this.props.fixture.teams.home.logo} className="float-left"></Image>
                    <h3>{this.props.fixture.teams.home.name}</h3>
                </Col>
-               <Col className="text-center score" xs={4}>
+               <Col className="text-center score text-primary" xs={4}>
                    <h3>{this.props.fixture.goals.home} : {this.props.fixture.goals.away}</h3>
                    <span title={this.props.fixture.fixture.date}>{this.props.fixture.fixture.status.elapsed}</span>
                </Col>
@@ -25,6 +25,11 @@ class InlineFixture extends React.Component{
                    <h3>{this.props.fixture.teams.away.name}</h3>
                </Col>
            </Row>
+            <Row>
+                <Col xs={12}>
+                    <ProgressBar now={(this.props.fixture.fixture.status.elapsed/90)*100} className="fixture-elapsed-bar" />
+                </Col>
+            </Row>
        </div>;
     }
 }
