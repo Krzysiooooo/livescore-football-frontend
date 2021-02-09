20e395ea8c7398fe57c309d2143d7d1892301000
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2021-02-09 17:41:50 +0100

    getLiveFixtures integration

--- src/components/InlineFixture/InlineFixture.js
+++ src/components/InlineFixture/InlineFixture.js
@@ -10,19 +10,19 @@ class InlineFixture extends React.Component{
     }
 
    render() {
-        return <div key={this.props.fixture.fixture_id} className="inline-fixture">
+        return <div className="inline-fixture">
            <Row>
                <Col xs={4}>
-                   <Image src={this.props.fixture.homeTeam.logo} className="float-left"></Image>
-                   <h3>{this.props.fixture.homeTeam.team_name}</h3>
+                   <Image src={this.props.fixture.teams.home.logo} className="float-left"></Image>
+                   <h3>{this.props.fixture.teams.home.name}</h3>
                </Col>
                <Col className="text-center score" xs={4}>
-                   <h3>{this.props.fixture.goalsHomeTeam} : {this.props.fixture.goalsAwayTeam}</h3>
-                   <span title={this.props.fixture.event_date}>{this.getHumanDate(this.props.fixture.event_date)}</span>
+                   <h3>{this.props.fixture.goals.home} : {this.props.fixture.goals.away}</h3>
+                   <span title={this.props.fixture.fixture.date}>{this.props.fixture.fixture.status.elapsed}</span>
                </Col>
                <Col className="text-right" xs={4}>
-                   <Image src={this.props.fixture.awayTeam.logo} className="float-right"></Image>
-                   <h3>{this.props.fixture.awayTeam.team_name}</h3>
+                   <Image src={this.props.fixture.teams.away.logo} className="float-right"></Image>
+                   <h3>{this.props.fixture.teams.away.name}</h3>
                </Col>
            </Row>
        </div>;
--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -8,13 +8,15 @@ class HomePage extends React.Component {
     constructor() {
         super();
         this.state = {fixtures: []};
-        BackendApi.getLiveFixtures().then(data => this.setState({fixtures: data.fixtures}));
+        BackendApi.getLiveFixtures().then(fixtures => {
+            this.setState({fixtures: fixtures})
+        });
     }
 
     render() {
         return <div>
             <h2>Games in play</h2>
-            {this.state.fixtures.map(fixture => <InlineFixture fixture={fixture} key={fixture.fixture_id}></InlineFixture>)}
+            {this.state.fixtures.map(fixture => <InlineFixture fixture={fixture} key={fixture.fixture.id}></InlineFixture>)}
         </div>
     }
 }
