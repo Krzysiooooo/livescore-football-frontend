f8c694b7f45d317b5c968bb2f62a3643ac7ec0dd
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2022-05-17 08:58:09 +0200

    wip

--- src/components/FixturesList/FixtureList.js
+++ src/components/FixturesList/FixtureList.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import BackendApi from "../../services/BackendApi";
 import InlineFixture from "../InlineFixture/InlineFixture";
-import {Col,Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
+import _ from "lodash";
+import Select from 'react-select'
 
 class FixtureList extends React.Component {
 
@@ -9,20 +11,47 @@ class FixtureList extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {fixtures: []};
+        this.state = {fixtures: [], leaguesList: [], selectedLeagues: []};
+            this.onFilterChange = this.onFilterChange.bind(this);
     }
+
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.props.date !== this.date) {
             this.date = this.props.date;
             BackendApi.getFixtureByDate(this.props.date).then(fixtures => {
-                this.setState({fixtures: fixtures})
+                const leaguesList = _.chain(fixtures).map(item => item.league.name).uniq().sort().map(item => ({
+                    value: item,
+                    label: item
+                })).value();
+                this.setState({fixtures: fixtures, leaguesList: leaguesList})
             });
         }
     }
 
+
+    onFilterChange(options){
+        this.setState({selectedLeagues: options});
+        return 0;
+    };
+
+
+
     render() {
         return <div id="fixture-list">
+            <Row>
+                <Col xs="12">
+                    <Select
+                        options={this.state.leaguesList}
+                        onChange={this.onFilterChange}
+                        isMulti/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {this.state.selectedLeagues.map(x => x.value).join()}
+                </Col>
+            </Row>
             <Row>
                 <Col xs="12">
                     {this.state.fixtures.map(fixture => <InlineFixture fixture={fixture}
